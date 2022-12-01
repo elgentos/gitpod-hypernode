e488b708d0927b02b8c07c26c14c49aef0ca8a57
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2022-12-01 21:39:54 +0100

    Update m2-install.sh

--- gitpod/m2-install.sh
+++ gitpod/m2-install.sh
@@ -3,7 +3,7 @@ sleep 12;
 mysql -e 'CREATE DATABASE IF NOT EXISTS magento;' &&
 pass=$(cat /data/web/.my.cnf  | grep pass | awk '{print$3}') && 
 url=$(gp url | awk -F"//" {'print $2'}) && url+="/" && 
-url="https://8002-"$url && 
+url="https://80-"$url && 
 if [ "${INSTALL_MAGENTO}" = "YES" ]; then php bin/magento setup:install --db-name='magento' --db-user='app' --db-password=$pass --base-url=$url --backend-frontname='admin' --admin-user=$MAGENTO_ADMIN_USERNAME --admin-password=$MAGENTO_ADMIN_PASSWORD --admin-email=$GITPOD_GIT_USER_EMAIL --admin-firstname='Admin' --admin-lastname='User' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='Europe/Amsterdam' --currency='EUR' --session-save='redis'; fi &&
 
 magerun2 module:disable Magento_Csp &&
@@ -20,4 +20,7 @@ php bin/magento config:set web/cookie/cookie_domain ".gitpod.io" &&
 magerun2 cache:flush &&
 redis-cli flushall &&
 
-touch $GITPOD_REPO_ROOT/gitpod/db-installed.flag
+touch $GITPOD_REPO_ROOT/gitpod/db-installed.flag &&
+
+ln -s /workspace/gitpod-hypernode/ /data/web/current  &&
+ln -s /data/web/current/pub /data/web/public
